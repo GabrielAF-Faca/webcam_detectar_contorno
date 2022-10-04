d8a9f1a30afe5c47a75d4def2dfd58ef5dd9cce6
Gabriel Azenha Fachim <101106957+GabrielAF-Faca@users.noreply.github.com>
2022-10-04 19:20:17 -0300

    Update detecta_contorno.py

--- detecta_contorno.py
+++ detecta_contorno.py
@@ -4,15 +4,15 @@ cap = cv2.VideoCapture(0)
 
 while True:
     _, frame = cap.read()
-    # convert to grayscale
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # create a binary thresholded image
+
     _, binary = cv2.threshold(gray, 85, 255, cv2.THRESH_BINARY)
-    # find the contours from the thresholded image
+
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # draw all contours
+
     image = cv2.drawContours(frame, contours, -1, (0, 255, 0), 2)
-    # show the images
+
     cv2.imshow("gray", gray)
     cv2.imshow("image", image)
     cv2.imshow("binary", binary)
